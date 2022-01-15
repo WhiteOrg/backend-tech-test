b328ab6ca10d3fabe09db9c89027f85fba89c734
Calvin Tapp <calvintapp@gmail.com>
2022-01-15 18:35:40 +0000

    implement handlers for author commands

---
+++ src/TechTest.Infrastructure/Handlers/Commands/AuthorCommandHandlers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using TechTest.Core.Entities;
+using TechTest.Core.Interfaces;
+using TechTest.Core.Models;
+
+namespace TechTest.Infrastructure.Handlers.Commands
+{
+    public class AuthorCommandHandlers : BaseCommandHandler, IRequestHandler<CreateAuthorCommand>, 
+        IRequestHandler<UpdateAuthorCommand, Author>, IRequestHandler<DeleteAuthorCommand>
+    {
+        public AuthorCommandHandlers(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public async Task<Unit> Handle(CreateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var newAuthor = new Author() { Name = request.Name };
+            await UnitOfWork.AuthorRepo.AddAsync(newAuthor);
+            await UnitOfWork.SaveAsync(cancellationToken);
+            return Unit.Value;
+        }
+
+        public async Task<Author> Handle(UpdateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var authorToUpdate = await UnitOfWork.AuthorRepo.GetAsync(request.Id);
+            if (authorToUpdate == null)
+            {
+                throw new Exception("Author not found, please ensure Author Id is provided and is valid.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(request.Name))
+            {
+                authorToUpdate.Name = request.Name;
+            }
+
+            UnitOfWork.AuthorRepo.Update(authorToUpdate);
+            await UnitOfWork.SaveAsync(cancellationToken);
+            return authorToUpdate;
+        }
+
+        public async Task<Unit> Handle(DeleteAuthorCommand request, CancellationToken cancellationToken)
+        {
+            UnitOfWork.AuthorRepo.Delete(request.Id);
+            await UnitOfWork.SaveAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/TechTest.Infrastructure/Handlers/Commands/BaseCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using TechTest.Core.Interfaces;
+
+namespace TechTest.Infrastructure.Handlers.Commands
+{
+    public class BaseCommandHandler
+    {
+        protected readonly IUnitOfWork UnitOfWork;
+
+        public BaseCommandHandler(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+    }
+}
