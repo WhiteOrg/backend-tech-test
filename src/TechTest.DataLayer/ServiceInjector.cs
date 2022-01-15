d21f205069197610c2bee92c71383147425987a3
Calvin Tapp <calvintapp@gmail.com>
2022-01-15 14:16:05 +0000

    init structure and first implementations

---
+++ src/TechTest.DataLayer/ServiceInjector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TechTest.DataLayer
+{
+    public static class ServiceInjector
+    {
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<LibrayDataContext>(options =>
+            {
+                options.UseInMemoryDatabase($"Library{Guid.NewGuid()}");
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
