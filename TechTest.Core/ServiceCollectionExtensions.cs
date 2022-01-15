bd5651e5c11f99b6c85b9c6eb7989b981f0cc867
Calvin Tapp <calvintapp@gmail.com>
2022-01-15 17:21:20 +0000

    add service collection extension for core services

---
+++ TechTest.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TechTest.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
